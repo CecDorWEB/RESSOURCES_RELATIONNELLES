00cbe1072dc8c05c3f035706959610f055a7b63f
CecDor <150656709+CecDorWEB@users.noreply.github.com>
2025-02-25 16:04:48 +0100

    init

---
+++ src/main/java/com/RESSOURCES_RELATIONNELLES/RessourcesRelationnellesApplication.java
@@ -0,0 +1,13 @@
+package com.RESSOURCES_RELATIONNELLES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessourcesRelationnellesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RessourcesRelationnellesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RESSOURCES_RELATIONNELLES/RessourcesRelationnellesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RESSOURCES_RELATIONNELLES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RessourcesRelationnellesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
