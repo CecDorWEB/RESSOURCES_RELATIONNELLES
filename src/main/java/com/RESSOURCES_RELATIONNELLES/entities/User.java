4e29b237d832cf9cf9d671cb4976945e5a9caae0
CecDor <150656709+CecDorWEB@users.noreply.github.com>
2025-02-26 11:52:42 +0100

    delete abstract classes

--- src/main/java/com/RESSOURCES_RELATIONNELLES/entities/User.java
+++ src/main/java/com/RESSOURCES_RELATIONNELLES/entities/User.java
@@ -4,22 +4,17 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "ROLE", length = 10)
-public abstract class User {
+public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
