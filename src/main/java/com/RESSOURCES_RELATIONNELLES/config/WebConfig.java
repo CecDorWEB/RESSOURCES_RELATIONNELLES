eb0f81f0da8494dea07401e4461370cfff0816ab
Tiramisuuus <lemeunie6u@etu.univ-lorraine.fr>
2025-02-26 16:24:10 +0100

    fix

--- src/main/java/com/RESSOURCES_RELATIONNELLES/config/WebConfig.java
+++ src/main/java/com/RESSOURCES_RELATIONNELLES/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/authentification/**", "/notAccess"); // Autorise certaines pages
+                .addPathPatterns("/**" )
+                .excludePathPatterns("/authentification/**", "/notAccess" , "/"); // Autorise certaines pages
     }
 }
